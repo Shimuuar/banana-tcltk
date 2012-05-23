aa19c82d58890cc47ef27d4c1edaa2fc21ce4f9d
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-05-23 22:33:38 +0400

    More combinators

--- Reactive/Banana/Extra.hs
+++ Reactive/Banana/Extra.hs
@@ -1,5 +1,6 @@
 module Reactive.Banana.Extra where
 
+import Control.Applicative
 import Reactive.Banana
 
 -- | 'scanl' like accumulation function
@@ -12,5 +13,21 @@ actimateWith f = reactimate . fmap f
 actimateWith_ :: IO () -> Event t a -> NetworkDescription t ()
 actimateWith_ f = reactimate . fmap (const f)
 
+actimateBhvWith :: (a -> IO ()) -> Behavior t a -> NetworkDescription t ()
+actimateBhvWith f bhv
+  = actimateWith f =<< changes bhv
+
 zipE :: (a -> b -> c) -> Behavior t a -> Event t b -> Event t c
 zipE f b e = fmap f b <@> e
+
+
+pairWith :: (a -> b -> c) -> Event t a -> Event t b -> Event t c
+pairWith f ea eb
+  = filterJust
+  $ fmap fini
+  $ scanE acc (Nothing,Nothing)
+  $ fmap Left ea `union` fmap Right eb
+  where
+    acc (_,b) (Left  a) = (Just a, b)
+    acc (a,_) (Right b) = (a, Just b)
+    fini (a,b) = f <$> a <*> b
