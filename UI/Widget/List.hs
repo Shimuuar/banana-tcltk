f02da41a8ef0d308422a3f6431de461f61f66e00
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-05-23 22:34:47 +0400

    Parametrize list with behavior instead of static list

--- UI/Widget/List.hs
+++ UI/Widget/List.hs
@@ -2,6 +2,7 @@ module UI.Widget.List (
   listWidget
   ) where
 
+import Control.Arrow
 import Control.Monad.Trans.Class
 
 import Reactive.Banana
@@ -12,37 +13,47 @@ import UI.TclTk.AST
 import UI.TclTk.Builder
 import UI.Reactive
 import UI.Widget
+import Debug.Trace
 
 
-listWidget :: Show a => Source -> [a] -> Gui t p (Widget t (Int,a))
-listWidget _   [] = do
-  nm <- frame [] (return ())
-  return $ Widget nm never
-listWidget src xs = do
+
+listWidget :: Show a => Source -> Behavior t [a] -> Gui t p (Widget t (Int,a))
+listWidget src bhvXs = do
   -- Events
-  e       <- lift $ addEventSource src
+  cmdEvt  <- lift $ addEventSource src
   initEvt <- getParameter
-  let evt = unions [ fmap (const (0, head xs)) initEvt
-                     -- It should use current behavior instead of hardcoded element
-                   , listEvents xs $ tclEvent e
-                   ]
-  -- UI
+  xsEvt   <- lift $ union (bhvXs <@ initEvt) <$> changes bhvXs
+  let lenEvt = length <$> xsEvt
+      evt    = listEvents xsEvt $ tclEvent cmdEvt
+      go     = cmd cmdEvt
+  ----------------------------------------
+  -- Build UI
+  lift $ actimateWith print lenEvt
   name <- frame [] $
     withPack PackLeft $ do
-      -- GUI
-      button [Text "<|" ] [] $ cmd e  ToBegin
-      button [Text "<<<"] [] $ cmd e (MoveBack 10)
-      button [Text "<"  ] [] $ cmd e (MoveBack 1)
-      nm <- label [ Text "0" , Width 10 ] []
-      label [ Text $ " / " ++ show (length xs) ] []
-      button [Text ">"  ] [] $ cmd e (MoveFwd  1)
-      button [Text ">>>"] [] $ cmd e (MoveFwd  10)
-      button [Text "|>" ] [] $ cmd e  ToEnd
+      --
+      button [Text "<|" ] [] $ go  ToBegin
+      button [Text "<<<"] [] $ go (MoveBack 10)
+      button [Text "<"  ] [] $ go (MoveBack 1)
+      -- labels
+      nm   <- label [ Width 10    ] []
+      _    <- label [ Text  " / " ] []
+      labN <- label []              []
+      actimateTcl src lenEvt $ do
+        configure labN $ LamOpt $ Text . show
+      actimateTcl src evt $ do
+        configure nm $ LamOpt $ \e ->
+          case e of
+            Just (i,_) -> Text $ show i
+            Nothing    -> Text "-"
+      --
+      button [Text ">"  ] [] $ go (MoveFwd  1)
+      button [Text ">>>"] [] $ go (MoveFwd  10)
+      button [Text "|>" ] [] $ go  ToEnd
       -- Network
-      actimateTcl src (fmap fst evt) $
-        configure nm (LamOpt $ \i -> Text (show i))
+      return ()
   -- Return data
-  return $ Widget name evt
+  return $ Widget name $ filterJust evt
 
 
 
@@ -53,6 +64,10 @@ data ListCmd
   | ToBegin
   | ToEnd
   deriving (Show)
+
+data Cursor a
+  = Cursor Int [a] Int
+  | Invalid
 
 instance Command ListCmd where
   encode (MoveFwd  n) = ["fwd",  show n]
@@ -66,20 +81,36 @@ instance Command ListCmd where
   decode _            = Nothing
 
 
-
--- | Behavior of a list
-listEvents :: [a] -> Event t ListCmd -> Event t (Int,a)
-listEvents [] _   = error "Empty list"
-listEvents xs evt
-  = fmap (\i -> (i, xs !! i)) $ scanE go 0 evt
+listEvents :: Event t [a] -> Event t ListCmd -> Event t (Maybe (Int,a))
+listEvents listEvt command
+  = fmap fini
+  $ scanE acc Invalid
+  $ fmap Left listEvt `union` fmap Right command
   where
-    go n (MoveFwd  d) = clip $ n + d
-    go n (MoveBack d) = clip $ n - d
-    go _  ToBegin     = 0
-    go _  ToEnd       = len - 1
     --
-    clip i | i < 0     = 0
-           | i >= len  = len -1
-           | otherwise = i
-    --
-    len = length xs
+    fini (Cursor _ xs i) = Just (i, xs !! i)
+    fini Invalid         = Nothing
+    -- Accumulate data
+    acc Invalid _
+      | trace "ACC Invalid" False = undefined
+    acc (Cursor len _ i) _
+      | traceShow ("ACC",len,i) False = undefined
+    acc _ (Left xs)
+      | traceShow ("LEFT: ",length xs) False = undefined
+    acc _ (Right c)
+      | traceShow ("RIGHT", c) False = undefined
+
+
+    acc Invalid (Left []) = Invalid
+    acc _       (Left xs) = trace "NEWCURSOR" Cursor (length xs) xs 0
+    acc Invalid _         = Invalid
+    acc (Cursor len xs n) (Right c) =
+      case c of
+        MoveFwd  d -> Cursor len xs $ clip len $ n + d
+        MoveBack d -> Cursor len xs $ clip len $ n - d
+        ToBegin    -> Cursor len xs 0
+        ToEnd      -> Cursor len xs $ len - 1
+    -- Clip out of range indices
+    clip len i | i < 0     = 0
+               | i >= len  = len -1
+               | otherwise = i
