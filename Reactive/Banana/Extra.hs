6c449fa6c49c6f338e8d2aaca2ec589897f7f0f2
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-05-23 13:22:08 +0400

    More combinators

--- Reactive/Banana/Extra.hs
+++ Reactive/Banana/Extra.hs
@@ -9,5 +9,8 @@ scanE fold x0 = accumE x0 . fmap (flip fold)
 actimateWith :: (a -> IO ()) -> Event t a -> NetworkDescription t ()
 actimateWith f = reactimate . fmap f
 
+actimateWith_ :: IO () -> Event t a -> NetworkDescription t ()
+actimateWith_ f = reactimate . fmap (const f)
+
 zipE :: (a -> b -> c) -> Behavior t a -> Event t b -> Event t c
 zipE f b e = fmap f b <@> e
