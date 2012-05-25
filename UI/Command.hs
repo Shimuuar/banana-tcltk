e6115740aefc76a0111f07cbbf4aea4c03e5f12a
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-05-25 16:39:51 +0400

    Add Command instance to Bool

--- UI/Command.hs
+++ UI/Command.hs
@@ -3,6 +3,9 @@ module UI.Command (
   , Cmd(..)
   ) where
 
+import Data.Char
+
+
 -- | Type class for command which could be serialized and deserialized
 --   strings
 class Command a where
@@ -31,3 +34,15 @@ instance Command Int where
   decode [s] = case reads s of [(i,"")] -> Just i
                                _        -> Nothing
   decode _   = Nothing
+
+instance Command Bool where
+  encode True  = ["true" ]
+  encode False = ["false"]
+  decode [c] =
+    case map toLower c of
+      "true"  -> Just True
+      "1"     -> Just True
+      "false" -> Just False
+      "0"     -> Just False
+      _       -> Nothing
+  decode _ = Nothing
