3d3f1934bd6f2f7378c819bc406a54d38a660158
'Alexey Khudyakov <alexey.skladnoy@gmail.com>
2013-07-24 16:32:44 +0400

    Inital partially working reimplementation

--- UI/Widget/List.hs
+++ UI/Widget/List.hs
@@ -1,5 +1,5 @@
 module UI.Widget.List (
-  listWidget
+  -- listWidget
   ) where
 
 import Reactive.Banana
@@ -13,40 +13,42 @@ import UI.Widget
 import UI.Command
 
 
-listWidget :: Frameworks t => EB t [a] -> GUI t p (TkName, Event t (Int,a))
-listWidget (EB evtXs bhvXs) = do
+listWidget :: Frameworks t => Event t [a] -> GUI t p (TkName, Event t (Int,a))
+listWidget eList = do
   -- Events
-  (pref,cmdEvt) <- addTclEvent
-  -- Function to transform event
-  let getEvents ixE = listEvents evtXs $ cmdEvt `union` (JumpTo <$> ixE)
-  ----------------------------------------
+
+--   (pref,cmdEvt) <- addTclEvent
+--   -- Function to transform event
+--   let getEvents ixE = listEvents evtXs $ cmdEvt `union` (JumpTo <$> ixE)
+--   ----------------------------------------
   -- Build UI
   frame [Fill FillX] $
     withPack PackLeft $ do
-      let cmd = Cmd pref
+      let toEvt x (_,e) = x <$ e
+      -- Buttons
       spacer
-      button [Text "<|" ] [] $ cmd  ToBegin
-      button [Text "<<<"] [] $ cmd (MoveBack 10)
-      button [Text "<"  ] [] $ cmd (MoveBack 1)
-      --
+      e1 <- toEvt ToBegin       <$> button [Text "<|" ] []
+      e2 <- toEvt (MoveBack 10) <$> button [Text "<<<"] []
+      e3 <- toEvt (MoveBack 1 ) <$> button [Text "<"  ] []
+--       --
       spacer
-      (_,evt,_) <- finiWidget . modifyWidgetM fst getEvents =<< entryInt [] [] 0
+--       (_,evt,_) <- finiWidget . modifyWidgetM fst getEvents =<< entryInt [] [] 0
       _       <- label [ Text  " / " ] []
       labN    <- label []              []
-      spacer
-      --
-      button [Text ">"  ] [] $ cmd (MoveFwd  1)
-      button [Text ">>>"] [] $ cmd (MoveFwd  10)
-      button [Text "|>" ] [] $ cmd  ToEnd
-      spacer
-      -- Actions
-      actimateTclB (length <$> bhvXs) $ do
+      actimateTcl (length <$> eList) $ do
         configure labN $ LamOpt $ Text . show
-      return evt
+      spacer
+      -- More buttons
+      e4 <- toEvt (MoveFwd 1 ) <$> button [Text ">"  ] []
+      e5 <- toEvt (MoveFwd 10) <$> button [Text ">>>"] []
+      e6 <- toEvt ToEnd        <$> button [Text "|>" ] []
+      spacer
+      -- OOPS
+      undefined
 
 
 
--- | Commands for a list
+-- Commands for a list
 data ListCmd
   = MoveFwd  Int
   | MoveBack Int
@@ -55,23 +57,35 @@ data ListCmd
   | ToEnd
   deriving (Show)
 
+-- Cursor for list of values
 data Cursor a
-  = Cursor Int [a] Int
+  = Cursor Int [a] Int          -- Length × list × position
   | Invalid
 
-instance Command ListCmd where
-  encode (MoveFwd  n) = ["fwd",  show n]
-  encode (MoveBack n) = ["back", show n]
-  encode (JumpTo   n) = ["jump", show n]
-  encode  ToBegin     = ["begin"]
-  encode  ToEnd       = ["end"]
-  decode ("fwd"  : n) = MoveFwd  <$> decode n
-  decode ("back" : n) = MoveBack <$> decode n
-  decode ("jump" : n) = JumpTo   <$> decode n
-  decode ["begin"]    = Just   ToBegin
-  decode ["end"]      = Just   ToEnd
-  decode _            = Nothing
 
+
+mergeEvent :: Event t (Either [a] ListCmd)
+           -> Event t (Cursor a)
+mergeEvent = scanE acc Invalid
+  where
+    acc Invalid (Left []) = Invalid
+    acc Invalid (Left xs) = Cursor (length xs) xs 0
+    acc Invalid _         = Invalid
+    acc (Cursor _   _  n) (Left xs) =
+      Cursor len xs $ clip len n where len = length xs
+    acc (Cursor len xs n) (Right c) =
+      case c of
+        MoveFwd  d -> go $ n + d
+        MoveBack d -> go $ n - d
+        JumpTo   d -> go   d
+        ToBegin    -> go   0
+        ToEnd      -> go $ len - 1
+      where
+        go = Cursor len xs . clip len
+    -- Clip out of range indices
+    clip len i | i < 0     = 0
+               | i >= len  = len -1
+               | otherwise = i
 
 listEvents :: Event t [a] -> Event t ListCmd -> Event t (Maybe (Int,a))
 listEvents listEvt command
